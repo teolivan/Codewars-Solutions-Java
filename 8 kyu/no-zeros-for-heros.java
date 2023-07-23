1fec48c4700d43062f506da724febb83d0f5d36c
olivia svensson <oliviateoliva@gmail.com>
2023-07-23 04:00:24 +0200

    add solution

---
+++ 8 kyu/no-zeros-for-heros.java
@@ -0,0 +1,24 @@
+/*Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+Zero alone is fine, don't worry about it. Poor guy anyway */
+
+//My Solution
+
+import java.math.*;
+public class NoBoring {
+    public static int noBoringZeros(int n) {
+      
+        String numStr = Integer.toString(n);
+        String trimmedStr = numStr.replaceAll("0*$", ""); 
+        return Integer.parseInt(trimmedStr);
+    }
+    
+}

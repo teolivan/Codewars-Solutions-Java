0df5a1211a3023a221baa6148d037e9fb5e13372
olivia svensson <oliviateoliva@gmail.com>
2023-07-23 04:07:27 +0200

    add solution

--- 8 kyu/no-zeros-for-heros.java
+++ 8 kyu/no-zeros-for-heros.java
@@ -15,10 +15,13 @@ Zero alone is fine, don't worry about it. Poor guy anyway */
 import java.math.*;
 public class NoBoring {
     public static int noBoringZeros(int n) {
-      
+      if(n == 0) {
+        return 0;
+      }
         String numStr = Integer.toString(n);
         String trimmedStr = numStr.replaceAll("0*$", ""); 
         return Integer.parseInt(trimmedStr);
+      
     }
     
 }

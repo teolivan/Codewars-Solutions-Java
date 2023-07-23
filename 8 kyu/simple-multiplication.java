4b9e636de6cc1ff4d2d203dbd62733e54f3f99bb
olivia svensson <oliviateoliva@gmail.com>
2023-07-23 03:41:36 +0200

    add solution

---
+++ 8 kyu/simple-multiplication.java
@@ -0,0 +1,13 @@
+/*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise. */
+
+//My Solution
+
+public class Sid {
+    public static int simpleMultiplication(int n) {
+      if(n % 2 == 0) {
+        return n * 8;
+      } else {
+       return n * 9;
+      }
+    }
+}

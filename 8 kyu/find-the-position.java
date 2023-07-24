92e56b0ec18dcc075b1790f9f59d4f4ada719695
olivia svensson <oliviateoliva@gmail.com>
2023-07-25 01:53:07 +0200

    add solution

---
+++ 8 kyu/find-the-position.java
@@ -0,0 +1,23 @@
+/*When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1" */
+
+//My solution
+
+public class Kata
+{
+  public static String position(char alphabet)
+  {
+    int count = 0;
+    String alphab = "abcdefghijklmnopqrstuvwxyz";
+    for(int i = 0; i < alphab.length(); i++) {
+      count++;
+      if (alphabet == alphab.charAt(i)) {
+        break;
+      }
+    }
+    return "Position of alphabet: " + String.valueOf(count);
+    }
+}

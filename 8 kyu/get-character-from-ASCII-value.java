917c3547e26c7a60a9db1f88f59de32e09b38ffd
olivia svensson <oliviateoliva@gmail.com>
2023-07-25 02:14:23 +0200

    add get character from ascii-value solution

---
+++ 8 kyu/get-character-from-ASCII-value.java
@@ -0,0 +1,16 @@
+/*Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+For ASCII table, you can refer to http://www.asciitable.com/ */
+
+//My solution
+
+public class Ascii {
+    public static char getChar(int c) {
+      return (char)c;
+    }
+  }
---
+++ 8 kyu/makeUpperCase.java
@@ -0,0 +1,11 @@
+/*Write a function which converts the input string to uppercase.
+
+ */
+
+ //My solution
+
+ class Upper{
+    public static String MakeUpperCase(String str){
+    return str.toUpperCase();
+    }
+  }

14d1c93feaa28f80b03f08ff1f9367b58656ced6
olivia svensson <oliviateoliva@gmail.com>
2023-09-06 23:28:48 +0200

    add solution

---
+++ 8 kyu/FIXME-Replace-all-dots.java
@@ -0,0 +1,20 @@
+/*The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.*/
+
+//My solution
+
+public class Dinglemouse {
+
+  public static String replaceDots(String str) {
+       String replacedStr = str.replace(".", "-");
+        return replacedStr;
+  }
+  
+}
